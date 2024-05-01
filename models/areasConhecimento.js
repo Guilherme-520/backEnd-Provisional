78598c9a45d5b09c6a8f41da12964e6eef6c311b
VG149 <92550652+VG149@users.noreply.github.com>
2024-05-01 14:12:15 -0300

    add da PK idAreaConhecimentos

--- models/areasConhecimento.js
+++ models/areasConhecimento.js
@@ -1,6 +1,10 @@
 const db = require("./index")
 
 const AreaConhecimentos = db.sequelize.define("AreaConhecimentos",{
+    idAreaConhecimentos:{
+        type: db.Sequelize.INTEGER,
+        primaryKey: true
+    },
     Nome:{
         type: db.Sequelize.STRING
     },
