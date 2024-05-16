4659d1e4ceaffd7081582dc88d6566771ab201c9
VG149 <92550652+VG149@users.noreply.github.com>
2024-05-16 18:33:39 -0300

    Create Autores.js

---
+++ routes/Autores.js
@@ -0,0 +1,54 @@
+const express = require('express')
+const router = express.Router()
+const Autores = require('../models/Autores');
+
+
+router.get('/', async(req, res)=>{
+    const com = await Autores.findAll();
+    res.json(com)
+})
+
+
+router.get('/:id', async(req, res)=>{
+  const id = req.params.id;
+  const com = await Autores.findByPk(id);
+  res.json(com)
+})
+
+router.post("/", async(req, res)=>{
+    const novo = req.body
+
+    try {
+        const autor = await Autores.create(novo)
+        res.json(autor)
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Erro ao cadastrar Autores '+ error.message });
+    }
+})
+
+
+router.put("/:id", async(req, res)=>{
+    const id = req.params.id;
+  const novosDados = req.body;
+
+  try {
+    const autor2 = await Autores.update(novosDados, {
+      where: {
+        id: id,
+      },
+    });
+
+    if (autor2[0] === 1) {
+      res.json({ message: 'Autores atualizado com sucesso.' });
+    } else {
+      res.status(404).json({ error: 'Autores não encontrado.' });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Erro ao atualizar Autores ' + error.message });
+  }
+})
+
+
+module.exports = router;
