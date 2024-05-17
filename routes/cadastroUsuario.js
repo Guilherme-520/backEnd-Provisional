360c8438ed8926393caae5b743e0036aaeec705a
RafaAlFr <rafa.alfr@gmail.com>
2024-05-16 23:29:30 -0300

    route UserProfile

--- routes/cadastroUsuario.js
+++ routes/cadastroUsuario.js
@@ -1,40 +1,45 @@
 const express = require('express');
 const router = express.Router();
 const UserProfiles = require('../models/userProfile')
-const Instituicoes = require('../models/instituicao')
-const Ouvinte = require('../models/ouvinte')
+const Ouvintes = require('../models/ouvinte')
+const Instituicoes = require('../models/instituicao');
+const { where } = require('sequelize');
 
 router.get('/', async (req, res) => {
     const uspfl = await UserProfiles.findAll();
     res.json(uspfl)
 });
 
+router.post("/", async (req, res) => {
 
-
-// Falta mexer ainda
-
-// router.post("/", async (req, res) => {
-//     const uspfl = req.body;
-//     const Nome = req.body.Nome
-//     const Email = req.body.Email
-//     const Senha = req.body.Senha
-//     const Instituicao = req.body.Instituicao
-//     const Curso = req.body.Curso
-//     const Periodo = req.body.Periodo
-
-//     try {
-
-//         const novoinst = await Instituicao.create(ninsti)
-
-
-
-
-//         const novo = await UserProfiles.create(uspfl)
-//         res.json(novo);
-//     } catch (error) {
-//         console.error(error);
-//         res.status(500).json({ error: 'Erro ao criar UserProfiles '+ error.message });
-//     }
-// });
+    const userprof = {
+        nome: req.body.Nome,
+        email: req.body.Email,
+        senha: req.body.Senha
+    }
+   
+    const userovnt = {
+        curso: req.body.Curso
+    }
+    const insti = {
+        nome: req.body.Nome,
+    }
+    try {
+        const nuserpfp = await UserProfiles.create({userprof});
+        console.log(nuserpfp.idUserProfile)
+        if (userovnt != null) {
+            const novnt = await Convidados.create({userovnt})
+            res.json(nusrpfl, novnt)
+            if(insti != null){
+                const ninst = await Convidados.create({insti})
+                res.json(nusrpfl, novnt, ninst)
+            }
+        }else{
+            console.error(error)
+        } 
+    } catch (error) {
+        
+    }
+});
 
 module.exports = router;
