a20f1305223bdc4323e85d419230d2ccbd81bf03
GCardos00 <cardos00dev@gmail.com>
2024-05-17 01:48:48 -0300

    Criando CorpoEditorialEvento

---
+++ models/corpoEditorialEvento.js
@@ -0,0 +1,14 @@
+const db = require("./index")
+
+const CorpoEditorialEventos = db.sequelize.define("CorpoEditorialEventos", {
+    idCorpoEditorialEventos:{
+        type: db.Sequelize.INTEGER,
+        primaryKey: true
+    },
+    idEventos:{
+        type: db.Sequelize.INTEGER
+    },
+    idCorpoEditoriais:{
+        type: db.Sequelize.INTEGER
+    }
+})
