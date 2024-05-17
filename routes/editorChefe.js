ced001dea7f459c65d6bd342bc68df88b1d5df5b
GCardos00 <cardos00dev@gmail.com>
2024-05-17 00:20:26 -0300

    Criando novas rotas

--- routes/editorChefe.js
+++ routes/editorChefe.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const router = express.Router()
 const EditorChefes = require('../models/editorChefe');
+const Instituicoes = require("../models/instituicao")
+const UserProfiles = require('../models/userProfile');
 
 
 router.get('/', async(req, res)=>{
@@ -16,20 +18,38 @@ router.get('/:id', async(req, res)=>{
 })
 
 router.post("/", async(req, res)=>{
-    const novo = req.body
 
-    try {
-        const edtc = await EditorChefes.create(novo)
-        res.json(edtc)
-    } catch (error) {
+    const userchef ={
+      linkLattes: req.body.linkLattes,
+      status: req.body.status
+    }
+    const userprof = {
+      nome: req.body.Nome,
+      email: req.body.Email,
+      senha: req.body.Senha
+    }
+    const insti = {
+        nome: req.body.Nome,
+    }
+  try {
+      const nuserpf = await UserProfiles.create({userprof});
+      console.log(nuserpfp.UserProfiles)
+      if (nuserpfp != null) {
+          const nuinsti = await Instituicoes.create({userovnt})
+          if (nuinsti != null) {
+            const nuuserchef = await Convidados.create({userovnt})
+            res.json(nuserpf, nuinsti, nuuserchef)
+          }
+        } 
+      } catch (error) {
         console.error(error);
         res.status(500).json({ error: 'Erro ao cadastrar EditorChefes '+ error.message });
-    }
+      }
 })
 
 
 router.put("/:id", async(req, res)=>{
-    const id = req.params.id;
+  const id = req.params.id;
   const novosDados = req.body;
 
   try {
