d79d3c6315f745198301e6b1f1869a1616ef6d8e
VG149 <92550652+VG149@users.noreply.github.com>
2024-09-11 02:08:58 -0300

    Update aceitar.js

--- routes/artigo/aceitar.js
+++ routes/artigo/aceitar.js
@@ -1,28 +1,27 @@
 const express = require('express');
 const router = express.Router();
-const { Avaliadores } = require("../../model/db");
+const { Eventos } = require("../../model/db");
 
-// Rota para o avaliador aceitar e iniciar a avaliação de um artigo
-router.post('/:idAvaliador/avaliar/:idArquivo', async (req, res) => {
+// Rota para o avaliador aceitar o evento
+router.post('/:idEvento/aceitar', async (req, res) => {
     try {
-        const { idAvaliador, idArquivo } = req.params;
+        const { idEvento } = req.params;
 
-         // Busca o registro de avaliação do avaliador para o artigo específico pelo ID do arquivo
-        const avaliador = await Avaliadores.findOne({ where: { id: idAvaliador, idArquivos: idArquivo } });
+        // Buscar o evento e definir o campo 'aceito' como verdadeiro (1)
+        const evento = await Eventos.findByPk(idEvento);
 
-        if (!avaliador) {
-            return res.status(404).json({ message: 'Avaliação não encontrada' });
+        if (!evento) {
+            return res.status(404).json({ message: 'Evento não encontrado' });
         }
 
-        // Atualizar o status da avaliação para "Em Avaliação", isso modifica o status de Avaliador 
-        avaliador.status = 'Em Avaliação';
-        await avaliador.save();
+        evento.aceito = 1; // Definindo o evento como aceito
+        await evento.save();
 
-        res.json({ message: 'Avaliação iniciada com sucesso!' });
+        res.json({ message: 'Evento aceito!' });
 
     } catch (error) {
-        console.error('Erro ao iniciar a avaliação:', error);
-        res.status(500).json({ message: 'Erro ao iniciar a avaliação' });
+        console.error('Erro ao aceitar o evento:', error);
+        res.status(500).json({ message: 'Erro ao aceitar o evento' });
     }
 });
 
